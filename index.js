2615acf1aeff81742f4fe8f557dd7f09993fb1f7
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2024-10-09 19:42:25 +0300

    v1

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("file is running....");
+
+document.getElementById('getLocationBtn').addEventListener('click', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, handleError, {
+            enableHighAccuracy: true, // Fetches a more accurate location
+            timeout: 5000,            // Time before error is thrown (optional)
+            maximumAge: 0             // Ensures that no cached location is used
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+});
+
+function showPosition(position) {
+    document.getElementById("lat-el").textContent='Latitude: ' + position.coords.latitude
+    //console.log();
+    document.getElementById("long-el").textContent='Longitude: ' + position.coords.longitude
+    //console.log();
+    document.getElementById("accuracy-el").textContent='accuarcy : '+position.coords.accuracy+' metres'
+    //console.log()
+}
+
+function handleError(error) {
+    console.error('Error code: ' + error.code + ', Message: ' + error.message);
+}
